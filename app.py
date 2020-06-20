650d3b9f57a7d499ba7670dec298c42a9419c991
李秉祐 <bigcookie.lee@gmail.com>
2020-06-20 10:33:32 +0800

    bigcookie: 讀取儲存的瀏覽人次

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host= "redis", port=6379)
 
+
 @app.route('/')
 def hello():
     redis.incr('hits')
     return 'Hello Docker learner! You have seen this page {0} times' . format (redis.get( 'hits' ))
 
+
+def load_saved_count():
+    return 1000
+
+
 if __name__ == "__main__":
+    redis.set('hits', load_saved_count())
     app.run(host= "0.0.0.0", debug=False)
